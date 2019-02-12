72b505b34bd740c3bd433a9a13d662710796e707
Mike Chaberski <mchaberski@gmail.com>
2019-02-12 11:33:23 -0500

    add classes and unit tests

---
+++ organizer/__init__.py
@@ -0,0 +1,49 @@
+# organizer module
+
+import os
+import os.path
+import shutil
+import logging
+from typing import Iterator
+
+
+def default_namer(expected_max: int=None) -> Iterator[str]:
+    if expected_max is None:
+        expected_max = 0
+    n = 0
+    fmt = "{:0" + str(len(str(expected_max))) + "d}"
+    while True:
+        yield fmt.format(n)
+        n += 1
+
+
+class Subdivider(object):
+
+    filter = None
+
+    def subdivide_max_files(self, directory: str, max_files_per_dir: int, dest_root:str=None):
+        assert max_files_per_dir > 0, "max files per dir must be positive integer"
+        dest_root = dest_root or directory
+        src_filenames = tuple()
+        for root, dirs, files in os.walk(directory):
+            src_filenames = files
+        if self.filter is not None:
+            src_filenames = list(filter(self.filter, src_filenames))
+        src_filenames = sorted(src_filenames)
+        num_files = len(src_filenames)
+        if not num_files:
+            return
+        namer = default_namer(num_files)
+        subdir_num_files = 0
+        current_subdir_name = namer.__next__()
+        os.makedirs(os.path.join(dest_root, current_subdir_name), exist_ok=True)
+        for filename in src_filenames:
+            if subdir_num_files >= max_files_per_dir:
+                current_subdir_name = namer.__next__()
+                os.makedirs(os.path.join(dest_root, current_subdir_name), exist_ok=True)
+                subdir_num_files = 0
+            dest_path = os.path.join(dest_root, current_subdir_name, filename)
+            shutil.move(os.path.join(directory, filename), dest_path)
+            subdir_num_files += 1
+
+
---
+++ tests/test_organizer.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import os.path
+import unittest
+import tempfile
+import organizer
+from organizer import Subdivider
+
+
+def _create_file(pathname, binary_data=None):
+    with open(pathname, 'wb') as ofile:
+        if binary_data:
+            ofile.write(binary_data)
+    assert os.path.isfile(pathname)
+
+
+class TestSubdivider(unittest.TestCase):
+
+
+    def assertFileContains(self, pathname, bindata):
+        fnf, actual = None, None
+        try:
+            with open(pathname, 'rb') as ifile:
+                actual = ifile.read()
+        except FileNotFoundError as e:
+            fnf = f"could not open {pathname} due to FileNotFoundError {e}"
+        if fnf:
+            self.fail(fnf)
+        self.assertEqual(bindata, actual, f"data from {pathname}")
+
+    def test_subdivide(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            source_dir = os.path.join(tmpdir, 'source')
+            os.makedirs(source_dir)
+            FILES = "abcdefghij"
+            contents = {}
+            for fn in FILES:
+                data =fn.encode('utf-8')
+                _create_file(os.path.join(source_dir, fn), data)
+                contents[fn] = data
+            subdivider = Subdivider()
+            subdivider.subdivide_max_files(source_dir, 3)
+            self.assertFileContains(os.path.join(source_dir, "00", "a"), contents["a"])
+            self.assertFileContains(os.path.join(source_dir, "00", "b"), contents["b"])
+            self.assertFileContains(os.path.join(source_dir, "00", "c"), contents["c"])
+            self.assertFileContains(os.path.join(source_dir, "01", "d"), contents["d"])
+            self.assertFileContains(os.path.join(source_dir, "01", "e"), contents["e"])
+            self.assertFileContains(os.path.join(source_dir, "01", "f"), contents["f"])
+            self.assertFileContains(os.path.join(source_dir, "02", "g"), contents["g"])
+            self.assertFileContains(os.path.join(source_dir, "02", "h"), contents["h"])
+            self.assertFileContains(os.path.join(source_dir, "02", "i"), contents["i"])
+            self.assertFileContains(os.path.join(source_dir, "03", "j"), contents["j"])
