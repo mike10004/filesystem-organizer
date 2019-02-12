e4f65bd12198845837fe21628e7c5dd0584cf4c5
Mike Chaberski <mchaberski@gmail.com>
2019-02-12 12:08:17 -0500

    add subdivide program

---
+++ subdivide.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+import logging
+import argparse
+import organizer
+
+
+_DEFAULT_MAX_FILES = 1024
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", metavar="ROOT", nargs="?", default=os.getcwd(), help="directory to subdivide")
+    parser.add_argument("--max-files", type=int, metavar="N", help="set max number of files per subdirectory")
+    parser.add_argument("--destination", metavar="DIR", help="set destination; default is ROOT")
+    parser.add_argument("--log-level", "-l", metavar="LEVEL", choices=('DEBUG', 'INFO', 'WARN', 'ERROR'), default='INFO', help="set log level")
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.__dict__[args.log_level])
+    subdivider = organizer.Subdivider()
+    max_files = _DEFAULT_MAX_FILES
+    if args.max_files is not None:
+        max_files = args.max_files
+    if max_files <= 0:
+        parser.error("max files must be positive integer")
+    subdivider.subdivide_max_files(args.directory, max_files)
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
